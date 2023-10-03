902e2161e46694e28d34d81e83d3a54bdb84acab
Ankit-0040 <ankitssingh1912@gmail.com>
2023-10-03 22:40:40 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's build calculator
         </a>
       </header>
     </div>
