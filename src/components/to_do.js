e60f6a8ab64ffbc216f0231b247f42b52c8db738
Ankit-0040 <ankitssingh1912@gmail.com>
2023-10-06 09:59:36 +0530

    updated_to_do

--- src/components/to_do.js
+++ src/components/to_do.js
@@ -1,9 +1,40 @@
-import React from 'react'
-
-function to_do() {
+import React, { useState } from 'react';
+import './to_do.css'
+function ToDo() {
+  const[value,setValue] = useState('');
+  const[listvalue,setlistvalue]=useState([]);
+  const handleAddtask = () =>{
+    setlistvalue((listvalue)=>{
+      const updatedList=[...listvalue,value];
+      setValue('');  
+      return updatedList;
+  })
+  console.log(value);
+  }
   return (
-    <div>to_do</div>
+    <>
+        <div className='to-do'>TO-DO List</div>
+        <div className='input'>
+        <div className='input_box_add_button'>
+        <input type='text' placeholder='Enter Task' className='input-textbox' value={value} onChange={(e) => setValue(e.target.value)}></input>
+        <button type= "button" className='button' onClick={() => handleAddtask()}>Add Task</button>
+         </div>
+         <div className='input_box_add_button'>
+          
+          {listvalue.map((data,i)=>{
+                return(
+                    <>
+                    <div key={i}>
+                        {data}
+                    </div>
+                    </>
+                )
+            })}
+         </div>
+        </div>
+    </>
+
   )
 }
 
-export default to_do
+export default ToDo
